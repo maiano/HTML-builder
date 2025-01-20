890cbe103d153f7c2f6b0a4df5de0e5a1f67073e
maiano <maiano@yandex.ru>
2025-01-20 23:16:56 +0100

    fix: correct spelling error

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const filePath = path.join(__dirname, 'text.txt');
 const outputStream = fs.createWriteStream(filePath);
 
-process.stdout.write('enter the text ("quit" for exit)\n');
+process.stdout.write('enter the text ("exit" for quit)\n');
 process.stdin.on('data', (data) => {
   const input = data.toString().trim();
   input === 'exit' ? exit() : outputStream.write(input + '\n');
